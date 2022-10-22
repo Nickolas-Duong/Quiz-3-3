389dc1edf9982632f1b64fbbe8afb831befc5b02
Nickolas Duong <nduong@cpp.edu>
2022-10-22 11:08:58 -0700

    Remove union, get choice instead

--- Functions.c
+++ Functions.c
@@ -2,61 +2,53 @@
 #include "Game.h"
 #include <time.h>
 #include <ctype.h>
-
 #define MAXLEN 10
 
 //playGame - user plays the game; game returns a value to prompt quit or win
 void playGame(void)
 {
-    union Data
-    {
-        int i;
-        char str[MAXLEN+1];
-    };
-    
-    union Data choice;
     int randomNum;
     time_t t;
+    char choice[MAXLEN+1];
     srand((unsigned) time(&t));
 
     //randomize correct number based on max number    
     randomNum = (rand() % max) + 1;
     //Loop within game while not 'q'
-    while(tolower((int)*choice.str) != 'q')
+    while(tolower((int)*choice) != 'q')
     {
         //union = get choice    
-        while(tolower((int)*choice.str) != 'q' && isdigit((int)*choice.str) != 1)
+        while(tolower((int)*choice) != 'q' && isdigit((int)*choice) != 1)
         {
             printf("Guess a random number between 1 and %d (press q to quit): ", max);
-            scanf("%s", choice.str);
-            if(tolower((int)*choice.str) != 'q' && isdigit((int)*choice.str) != 1)
+            scanf("%s", choice);
+            if(tolower((int)*choice) != 'q' && isdigit((int)*choice) != 1)
             {
                 printf("Invalid input, please try again\n\n");
             }
         }
 
         //if choice != 'q'
-        if(tolower((int)*choice.str) != 'q')
+        if(tolower((int)*choice) != 'q')
         {
             //convert to int
-            int temp = atoi(choice.str);
-            choice.i = temp;
+            int temp = atoi(choice);
 
             //if choice is lower
-            if(choice.i < randomNum && choice.i > 0)
+            if(temp < randomNum && temp > 0)
             {
                 printf("Your number is lower than the target number. \n");
             }
             //if choice is higher
-            else if(choice.i > randomNum && choice.i <= max)
+            else if(temp > randomNum && temp <= max)
             {
                 printf("You number is higher than the target number. \n");
             }
             //if choice is correct
-            else if(choice.i == randomNum)
+            else if(temp == randomNum)
             {
                 printf("You guessed the correct number! \n");
-                strcpy(*choice.str, 'q');
+                
             }
             //if choice is not accepted
             else
@@ -88,7 +80,6 @@ void changeMaxNum(void)
         {
             printf("Invalid value, please try again...\n\n");
         }
-        *temp = "";
     }
 
     max = atoi(temp);
