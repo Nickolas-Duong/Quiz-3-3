ca8868a2f705bfecd441671374da82b2b7eeab2a
Nickolas Duong <nduong@cpp.edu>
2022-10-22 08:44:31 -0700

    Add Function.c for functions

---
+++ Functions.c
@@ -0,0 +1,19 @@
+//Functions used by program
+
+//playGame - user plays the game; game returns a value to prompt quit or win
+    //Loop within game
+    //randomize correct number based on max number
+    //union = get choice
+    //if union == q
+        //quit game (return 0)
+
+    //else
+        //if union > randNum
+            //print guess is too high
+        //elif union < randNum
+            //print guess is too low
+        //else
+            //print you guessed correctly!
+            //return 1
+
+//changeMaxNum - user changes max number the game uses
